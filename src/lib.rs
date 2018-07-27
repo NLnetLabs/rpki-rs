cec85b577a30226f5ea7a8a683117a854d6c6004
Martin Hoffmann <hn@nvnc.de>
2018-07-27 14:22:22 +0200

    Split from routinator.

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+//! All things RPKI.
+//!
+//! The _Resource Public Key Infrastructure_ (RPKI) is an application of
+//! PKI to Internet routing security. It allows owners of IP address prefixes
+//! to publish cryptographically signed associations of their prefixes to
+//! autonomous systems, allowing the validation of the origin of a route
+//! announcement in BGP.
+//!
+//! This crate contains types and functionality useful for building RPKI
+//! applications.
+extern crate base64;
+#[macro_use] extern crate ber;
+extern crate bytes;
+extern crate chrono;
+#[macro_use] extern crate failure;
+#[macro_use] extern crate log;
+extern crate ring;
+extern crate untrusted;
+
+pub mod asres;
+pub mod cert;
+pub mod crl;
+pub mod ipres;
+pub mod manifest;
+pub mod roa;
+pub mod rsync;
+pub mod sigobj;
+pub mod tal;
+pub mod x509;
+
