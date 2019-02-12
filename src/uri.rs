e757cbc3973769a7ec91af9224cd6869b607b927
Tim Bruijnzeels <tim@nlnetlabs.nl>
2019-02-12 12:09:27 +0100

    Fixes a bug in the is_parent_of function, where empty paths in the potential parent were not processed properly.

--- src/uri.rs
+++ src/uri.rs
@@ -153,10 +153,11 @@ impl Rsync {
     /// Returns true if this uri is a directory and it contains the other
     /// uri.
     pub fn is_parent_of(&self, other: &Rsync) -> bool {
-        self.ends_with("/") &&
         self.module == other.module &&
-        other.path.len() > self.path.len() &&
-        other.path.starts_with(self.path.as_ref())
+        (self.path.is_empty() || (
+            self.ends_with("/") &&
+            other.path.starts_with(self.path.as_ref())
+        ))
     }
 
     pub fn encode_general_name<'a>(&'a self) -> impl encode::Values + 'a {
