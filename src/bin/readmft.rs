14d5f00301e8dddbe760efe04b8ad78a2be80af3
Martin Hoffmann <hn@nvnc.de>
2019-03-08 14:18:58 +0100

    Add readmft binary.

---
+++ src/bin/readmft.rs
@@ -0,0 +1,38 @@
+extern crate rpki;
+
+use std::{env, fs};
+use std::io::Read;
+use rpki::manifest::Manifest;
+
+
+fn main() {
+    let path = match env::args().nth(1) {
+        Some(path) => path,
+        None => {
+            println!("Usage: readmft <path>");
+            return
+        }
+    };
+    let mut file = match fs::File::open(path) {
+        Ok(file) => file,
+        Err(err) => {
+            println!("Can’t open file: {}", err);
+            return;
+        }
+    };
+    let mut data = Vec::new();
+    if let Err(err) = file.read_to_end(&mut data) {
+        println!("Can’t read file: {}", err);
+        return;
+    }
+
+    let _cert = match Manifest::decode(data.as_ref(), true) {
+        Ok(cert) => cert,
+        Err(err) => {
+            println!("Can’t decode manifest: {}", err);
+            return
+        }
+    };
+}
+
+
