c802e4310aaf3f507481fd52067724f83c3e9d24
Tim Bruijnzeels <tim@nlnetlabs.nl>
2022-01-18 10:05:01 +0100

    ID certificate support (#190)
    * Add 'remote' module.
    * Parse identity certificates.
    * Create ID TA certificate.

---
+++ src/remote/mod.rs
@@ -0,0 +1,19 @@
+//! Remote access protocols.
+//!
+//! This module contains types and procedures to support the communication
+//! protocols used between CAs and their parents and publication server.
+//! 
+//! Relevant RFCs:
+//! RFC 8183 out-of-band ID exchanges
+//! RFC 8181 publication protocol
+//! RFC 6492 provisioning protocol (up/down)
+//! 
+//! In addition to this there is an ongoing effort to make new version
+//! standards for these protocols in order to support needed features
+//! which are currently missing.
+
+#![cfg(feature = "remote")]
+
+//--- Modules
+//
+pub mod idcert;
