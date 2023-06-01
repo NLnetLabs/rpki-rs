2b19d2201657ea72beb8b6b25b292b7125ca3da2
Martin Hoffmann <martin@nlnetlabs.nl>
2023-06-01 13:13:57 +0200

    Very limited documentation for the new module.

--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -1,3 +1,8 @@
+//! Types for AS and IP adress resources.
+//!
+//! This module cotains the basic types for representing resources protected
+//! by RPKI: [`Asn`] for autonomous system numbers, [`Prefix`] for IP address
+//! prefixes, and [`MaxLenPrefix`] for prefixes with a prefix length range.
 
 pub use self::addr::{MaxLenPrefix, Prefix};
 pub use self::asn::{Asn, SmallAsnSet};
